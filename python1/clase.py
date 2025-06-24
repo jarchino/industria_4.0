c3dd1094e6f96bbb959cbdf31b603691419e711d
jarchino <jadriangro@gmail.com>
2025-06-24 13:51:18 -0700

    use una funcion que retorna datos

--- python1/clase.py
+++ python1/clase.py
@@ -4,9 +4,21 @@ vehiculos=[
     {"marca":"mazda", "modelo":"mx-5", "año":2024}
 ]
 
+# def mostrar_vehiculos_por_año(lista,año):
+#     for v in lista:
+#         if v['año'] == año:
+#             print(f"{v['marca']} {v['modelo']} ({v['año']})")
+            
+# mostrar_vehiculos_por_año(vehiculos,2020)
+
 def mostrar_vehiculos_por_año(lista,año):
+    resultado =[]
     for v in lista:
         if v['año'] == año:
-            print(f"{v['marca']} {v['modelo']} ({v['año']})")
-            
-mostrar_vehiculos_por_año(vehiculos,2020)
+            resultado.append(v)
+    return resultado
+
+vehiculos_filtrados = mostrar_vehiculos_por_año(vehiculos, 2024)
+
+for v in vehiculos_filtrados:
+    print(f"{v['marca']} {v['modelo']} ({v['año']})")
