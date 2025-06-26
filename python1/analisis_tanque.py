d5d574aed871ed521a7290c10e7fc7b3bbdd5549
jarchino <jadriangro@gmail.com>
2025-06-26 11:26:11 -0700

    se hizo analizo el documento csv

--- python1/analisis_tanque.py
+++ python1/analisis_tanque.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 #leer archivo csv
@@ -17,6 +18,40 @@ nivel_max = df["Nivel (litros)"].max()
 print(f"\nNivel máximo: {nivel_max} litros")
 
 #Tiempo en el que se estuvo lleno
-tiempos_lleno = df[df["Nivel (litros)"] == 100]["Tiempo (s)"]
+tiempos_lleno = df[df["Nivel (litros)"] == nivel_max]["Tiempo (s)"]
 print("\nTiempos en los que estuvo lleno:")
 print(tiempos_lleno.to_list())
+
+df.plot(x="Tiempo (s)", y="Nivel (litros)", marker='o', title="Nivel de tanque a lo largo del tiempo")
+
+plt.tight_layout() 
+plt.show()
+
+def clasificar_estado(nivel):
+    nivel_porciento = (nivel / nivel_max) * 100
+    if nivel_porciento == 0:
+        return "Vacío"
+    elif nivel_porciento <= 40:
+        return "Bajo"
+    elif nivel_porciento <= 70:
+        return "Medio"
+    elif nivel_porciento < 100:
+        return "Alto"
+    else:
+        return"Lleno"
+    
+#Creal columna estado
+df["Estado"] = df["Nivel (litros)"].apply(clasificar_estado)
+
+#Contar cuantas veces estuvo en cada estado
+print("\nEstados del tanque: ")
+estado_ordenado = ["Vacío","Bajo","Medio","Alto","Lleno"]
+print(df["Estado"].value_counts().reindex(estado_ordenado).fillna(0).astype(int))
+
+# Calcular duración total en cada estado
+duraciones = df["Estado"].value_counts().reindex(estado_ordenado).fillna(0).astype(int)
+
+print("\nDuración total por estado (en segundos):")
+for estado, segundos in duraciones.items():
+    print(f"{estado}: {segundos} segundos")
+
